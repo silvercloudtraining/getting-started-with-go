6c751f156adf6254e26fab5363ec4a32ba25c8c3
Michael Van Sickle <mvansickle1@gmail.com>
2019-08-28 11:25:24 -0400

    first two demos

---
+++ 02-basic-webservice/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("content-type", "application/json")
+		name := r.URL.Query().Get("name")
+		username := r.URL.Query().Get("username")
+		m := map[string]string{"name": name,
+			"username": username}
+		enc := json.NewEncoder(w)
+		err := enc.Encode(m)
+		if err != nil {
+			log.Print(err)
+		}
+	})
+
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# programming_with_go
+# Getting Started with Go
+
+This repository holds the demos and examples that are used in the [Getting Started with Go](https://silvercloudtraining.com/p/getting-started-with-go) course by [Silver Cloud Training](https://silvercloudtraining.com)
